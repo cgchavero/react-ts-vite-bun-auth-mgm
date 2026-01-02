c021530240c826d9fa8e4309f2833e7996a9a311
Cesar Granjeno GitHub <cgchavero@gmail.com>
2026-01-01 18:30:39 -0600

    Add Barrel pattern to Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Card from './Components/Card/Card'
+import { Card } from './Components'
 
 function App() {
   const [count, setCount] = useState(0)
