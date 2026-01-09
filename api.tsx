25121b9d9d33ac8b7aa40572a1a7c4fa8cb78f55
Cesar Granjeno GitHub <cgchavero@gmail.com>
2026-01-09 03:54:11 -0600

    Add CompanyPage

--- api.tsx
+++ api.tsx
@@ -1,23 +1,34 @@
 import axios from "axios"
-import type { CompanySearch } from "./company"
+import type { CompanyProfile, CompanySearch } from "./company"
 
 interface SearchResponse {
     data: CompanySearch[]
 }
 
-export const searchCompanies = async(query: string) => {
-    try{
+export const searchCompanies = async (query: string) => {
+    try {
         const data = await axios.get<SearchResponse>(
             `https://financialmodelingprep.com/stable/search-name?query=${query}&limit=10&exchange=NASDAQ&apikey=${import.meta.env.VITE_REACT_APP_API_KEY}`
         )
         return data
     } catch (error) {
-        if(axios.isAxiosError(error)) {
+        if (axios.isAxiosError(error)) {
             console.log('Axios error message: ', error.message)
             return error.message
         } else {
             console.log('Axios unexpected error: ', error)
             return 'An unexpected Axios error has occured.'
         }
+    }
+}
+
+export const getCompanyProfile = async (query: string) => {
+    try {
+        const data = await axios.get<CompanyProfile[]>(
+            `https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${import.meta.env.VITE_REACT_APP_API_KEY}`
+        )
+        return data
+    } catch (error: any) {
+        console.log('Axios error message from API: ', error.message)
     }
 }
